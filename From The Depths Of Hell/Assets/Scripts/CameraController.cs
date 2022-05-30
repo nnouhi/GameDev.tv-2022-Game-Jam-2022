aa3b593c2c56a92b3abd3307cf5f558e767cdb39
ChristoforosNicolaou <63289544+ChristoforosNicolaou@users.noreply.github.com>
2022-05-30 22:26:16 +0300

    Finished level design, added background and environment

--- From The Depths Of Hell/Assets/Scripts/CameraController.cs
+++ From The Depths Of Hell/Assets/Scripts/CameraController.cs
@@ -7,16 +7,32 @@ public class CameraController : MonoBehaviour
     [SerializeField] private GameObject followingObject;
     [SerializeField] private float range = 4.0f;
     [SerializeField] private float offset = 4.0f;
+    [SerializeField] private AudioClip theme;
+    [SerializeField] private AudioClip intro;
+    [SerializeField] private AudioClip ending;
+    [SerializeField] private MainMenu menu;
+    private AudioSource musicPlayer;
+    private bool gameStarted = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        musicPlayer = GetComponent<AudioSource>();
+        musicPlayer.clip = intro;
+        musicPlayer.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!menu.InMenu() && !gameStarted)
+        {
+            gameStarted = true;
+            musicPlayer.Stop();
+            musicPlayer.clip = theme;
+            musicPlayer.Play();
+        }
+
         if (followingObject)
         {
             float objectY = followingObject.transform.position.y;
@@ -25,5 +41,12 @@ public class CameraController : MonoBehaviour
                 this.transform.position = new Vector3(this.transform.position.x, objectY + offset, this.transform.position.z);
             }
         }
+    }
+
+    public void EndGame()
+    {
+        musicPlayer.Stop();
+        musicPlayer.clip = ending;
+        musicPlayer.Play();
     }
 }
